56261beda7541901181ad79d18f770579b6ec90a
Hugo Leonardo <59965105+hugolps@users.noreply.github.com>
2020-07-31 15:25:31 -0300

    Update

--- src/index.js
+++ src/index.js
@@ -1,28 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import App from './pages/Home/App';
 
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import CadastroVideo from './pages/cadastro/Video'
-import CadastroCategoria from './pages/cadastro/Categoria'
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
 
-
-
-const Pagina404 = () => (<div>Página 404</div>)
+const Pagina404 = () => (<div>Página 404</div>);
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path="/" component={App} exact/>
-      <Route path="/cadastro/video" component={CadastroVideo}/>
-      <Route path="/cadastro/categoria" component={CadastroCategoria}/>
+      <Route path="/" component={App} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
       <Route component={Pagina404} />
 
     </Switch>
   </BrowserRouter>,
 
-  document.getElementById('root')
+  document.getElementById('root'),
 );
-
-
